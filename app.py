bf55aecb76d7dafd14241a674fa40a85ce463484
Chuck Underwood <chillieguy@live.com>
2017-04-17 16:13:04 -0700

    Move response line /sms

--- app.py
+++ app.py
@@ -62,8 +62,8 @@ def sms():
     if counter > 1:
         # When counter is greater than one as for response
         if request.values.get('Body') == '1':
-            message = client.api.account.messages.create(to="+15416391136", from_="+15417145139",body="{} is requesting a call back. \nChuck will call you back immediately if possible.  Otherwise he will call ASAP.".format(sender))
-            message =  "Call request sent to Chuck for {}".format(sender)
+            message = client.api.account.messages.create(to="+15416391136", from_="+15417145139",body="{} is requesting a call back.".format(sender)
+            message =  "Call request sent to Chuck for {}.\nChuck will call you back immediately if possible.  Otherwise he will call ASAP".format(sender)
             # Moved to seperate function
             #junk = call()
         elif request.values.get('Body') == '2':
